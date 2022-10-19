817159183fa3002e9785109037721ecb1b668482
Kirti180 <kirti182001@gmail.com>
2022-10-19 12:43:09 +0530

    completed

--- login.js
+++ login.js
@@ -1,2 +1,11 @@
 let registerd id="abc@gmail.com"
 let registerd password="123456789"
+
+let log_reg="abc@gmail.com"
+let log_pass="123456789"
+
+if(registerd id==log_reg && registerd password==log_pass){
+    console.log("login successful")
+}else{
+    console.log("failed")
+}
